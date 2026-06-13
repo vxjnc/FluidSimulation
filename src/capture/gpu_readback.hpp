15a6a56b314f07d519596d493d9e5689c4b96750
vxjnc <vxjnc0@gmail.com>
2026-06-13 15:54:05 +0400

    refactor: hoist State template out of request for IDE type hints

--- src/capture/gpu_readback.hpp
+++ src/capture/gpu_readback.hpp
@@ -12,29 +12,29 @@ namespace GpuReadback {
 
     inline uint32_t alignUp(uint32_t v, uint32_t align) { return (v + align - 1) & ~(align - 1); }
 
+    template <typename Callback> struct State {
+        wgpu::raii::Buffer staging;
+        uint64_t size;
+        Callback callback;
+    };
+
     template <typename T = std::byte, typename Callback>
         requires ReadbackCallback<T, Callback>
     inline void request(wgpu::Buffer buffer, uint64_t size, Callback callback) {
         WGPUContext& ctx = WGPUContext::instance();
 
-        struct State {
-            wgpu::raii::Buffer staging;
-            uint64_t size;
-            Callback callback;
-        };
-
         wgpu::BufferDescriptor desc{};
         desc.size = size;
         desc.usage = wgpu::BufferUsage::CopyDst | wgpu::BufferUsage::MapRead;
 
-        auto* state = new State{ctx.device().createBuffer(desc), size, std::move(callback)};
+        auto* state = new State<Callback>{ctx.device().createBuffer(desc), size, std::move(callback)};
 
         wgpu::raii::CommandEncoder enc = ctx.device().createCommandEncoder({});
         enc->copyBufferToBuffer(buffer, 0, *state->staging, 0, size);
         wgpu::raii::CommandBuffer cmd = enc->finish({});
         ctx.queue().submit(1, &*cmd);
 
-        mapAsync<T>(state);
+        mapAsync<T, Callback>(state);
     }
 
     template <typename T = std::byte, typename Callback>
@@ -46,17 +46,11 @@ namespace GpuReadback {
         uint32_t bytesPerRow = alignUp(w * 4, 256);
         uint64_t size = static_cast<uint64_t>(bytesPerRow) * h;
 
-        struct State {
-            wgpu::raii::Buffer staging;
-            uint64_t size;
-            Callback callback;
-        };
-
         wgpu::BufferDescriptor desc{};
         desc.size = size;
         desc.usage = wgpu::BufferUsage::CopyDst | wgpu::BufferUsage::MapRead;
 
-        auto* state = new State{ctx.device().createBuffer(desc), size, std::move(callback)};
+        auto* state = new State<Callback>{ctx.device().createBuffer(desc), size, std::move(callback)};
 
         wgpu::TexelCopyTextureInfo src{};
         src.texture = texture;
@@ -75,14 +69,14 @@ namespace GpuReadback {
         wgpu::raii::CommandBuffer cmd = enc->finish({});
         ctx.queue().submit(1, &*cmd);
 
-        mapAsync<T>(state);
+        mapAsync<T, Callback>(state);
     }
 
-    template <typename T, typename State> inline void mapAsync(State* state) {
+    template <typename T, typename Callback> inline void mapAsync(State<Callback>* state) {
         wgpu::BufferMapCallbackInfo info{};
         info.mode = wgpu::CallbackMode::AllowSpontaneous;
         info.callback = [](WGPUMapAsyncStatus status, WGPUStringView, void* userdata1, void*) {
-            State* s = static_cast<State*>(userdata1);
+            auto* s = static_cast<State<Callback>*>(userdata1);
             if (status == wgpu::MapAsyncStatus::Success) {
                 const auto* ptr = static_cast<const T*>(s->staging->getConstMappedRange(0, s->size));
                 s->callback(std::span<const T>(ptr, s->size / sizeof(T)));
